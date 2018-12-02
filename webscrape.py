d371cdde271b895ac0ae183c7b6dea27fd0129ff
Zakariya Sattar <zakariyasattar03@gmail.com>
2018-12-02 12:23:06 -0600

    finish math

--- webscrape.py
+++ webscrape.py
@@ -25,6 +25,7 @@ def formatDate(str):
         finalString = currentDay + ", " + month + " " + str[indexInString+1:]
     return finalString
 
+
 now = datetime.datetime.now()
 dates = []
 href = []
@@ -33,6 +34,7 @@ finalHw = []
 html = urlopen("https://sites.google.com/a/d219.org/ms-madigan/unit-4-polynomial-functions").read();
 soup = BeautifulSoup(html, "html.parser");
 table = soup.find("table", attrs={"border": "1"})
+
 
 counter = 0
 for row in table.findAll('tr'):
@@ -45,31 +47,19 @@ for row in table.findAll('tr'):
 
     for x in col[2].findAll('a'):
         row = []
+        row.append(val.replace("\xa0", ""))
         for y in col[2].findAll('a'):
             row.append(y)
-            row.append(val)
         href.append(row)
 
-
-    a = col[2].text
-    #print(a[a.find("HW:")])
-
-    for char in range(len(a)):
-        if(a[char] == "H" and char + 1 < len(a)):
-            if(a[char + 1] == "W:"):
-                print("hello")
-
     for d in dates:
-        #today = formatDate(now.strftime("%m/%d"))
-        today = "Tues, Nov 27"
+        # today = formatDate(now.strftime("%m/%d"))
+        # today = "Tues, Nov 27"
+        today = "Wed, Nov 28"
 
         if(d == today or d[1:] == today):
-            print()
-
-for link in href:
-    finalHw.append(link[len(link) - 2])
-print(href[1])
-
-with open("data.csv", "a") as csv_file:
- writer = csv.writer(csv_file)
- writer.writerow([formatDate(now.strftime("%m/%d"))])
+            for link in href:
+                finalHw.append([link[0], link[len(link) - 1]])
+            for date in finalHw:
+                if(date[0] == d or date[0] == d[1:]):
+                    print("today's homework for math is: " + str(date[1]))
