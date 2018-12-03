7b6f6e464e036e0f40092849117660fbd16c19d5
Zakariya Sattar <zakariyasattar03@gmail.com>
2018-12-02 18:19:35 -0600

    added run.sh file to automatically execute script

--- math.py
+++ math.py
@@ -35,31 +35,33 @@ html = urlopen("https://sites.google.com/a/d219.org/ms-madigan/unit-4-polynomial
 soup = BeautifulSoup(html, "html.parser");
 table = soup.find("table", attrs={"border": "1"})
 
+def main():
+    counter = 0
+    for row in table.findAll('tr'):
+        counter+=1
+        col = row.findAll('td')
+        val = col[0].string
 
-counter = 0
-for row in table.findAll('tr'):
-    counter+=1
-    col = row.findAll('td')
-    val = col[0].string
+        if(counter > 1 and val != None):
+            dates.append(val)
 
-    if(counter > 1 and val != None):
-        dates.append(val)
+        for x in col[2].findAll('a'):
+            row = []
+            row.append(val.replace("\xa0", ""))
+            for y in col[2].findAll('a'):
+                row.append(y)
+            href.append(row)
 
-    for x in col[2].findAll('a'):
-        row = []
-        row.append(val.replace("\xa0", ""))
-        for y in col[2].findAll('a'):
-            row.append(y)
-        href.append(row)
+        for d in dates:
+            # today = formatDate(now.strftime("%m/%d"))
+            # today = "Tues, Nov 27"
+            today = "Wed, Nov 28"
 
-    for d in dates:
-        # today = formatDate(now.strftime("%m/%d"))
-        # today = "Tues, Nov 27"
-        today = "Wed, Nov 28"
+            if(d == today or d[1:] == today):
+                for link in href:
+                    finalHw.append([link[0], link[len(link) - 1]])
+                for date in finalHw:
+                    if(date[0] == d or date[0] == d[1:]):
+                        return ("today's homework for math is: " + str(date[1]))
 
-        if(d == today or d[1:] == today):
-            for link in href:
-                finalHw.append([link[0], link[len(link) - 1]])
-            for date in finalHw:
-                if(date[0] == d or date[0] == d[1:]):
-                    print("today's homework for math is: " + str(date[1]))
+print(main())
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python math.py >> index.html
